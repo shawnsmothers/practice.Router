60e170f2e26dd8365e0b22b5c6d280b78b686ab5
shawnsmothers <59694659+shawnsmothers@users.noreply.github.com>
2020-10-20 08:59:11 -0500

    first

---
+++ src/components/CampsiteInfoComponent.js
@@ -0,0 +1,70 @@
+import React from "react";
+import {Card, CardImg,CardText,CardTitle, CardBody} from 'reactstrap';
+
+
+
+    function RenderCampsite({campsite}){
+        return (
+            <div className= "col-md-5 m-1">
+            <Card>
+            <CardImg top src={campsite.image} alt={campsite.name}/>
+             
+            <CardBody>
+                 <CardTitle>
+                     {campsite.name}
+                 </CardTitle>
+                 <CardText>
+                     {campsite.description}
+                 </CardText>
+
+             </CardBody>
+         </Card>
+
+            </div>
+        )
+    }
+function RenderComments ({comments}){
+    if (comments){
+        return(
+            <div className="col-md-5 m-1">
+                <h4>comments</h4>
+                {comments.map((comment) =>(
+                    <div key={comment.id}>
+                        
+                            {comment.text}
+                        
+                        <p>--{" "}{comment.author}{", "}
+                    {new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit'}).format(new Date(Date.parse(comment.date)))}</p> 
+                        
+                     
+                   
+                  
+
+                    </div>
+                    ))}
+
+             </div>
+
+             )
+            }
+            }
+        
+
+function CampsiteInfo (props){
+    if (props.campsite){
+        return(
+            <div className="container">
+            <div className="row">
+                <RenderCampsite campsite={props.campsite}/>
+                <RenderComments comments={props.campsite.comments}/>
+            </div>
+            </div>
+        )
+
+    } else{
+        return <div/>
+    }
+}
+
+
+export default CampsiteInfo;
---
+++ src/components/MainComponent.js
@@ -0,0 +1,46 @@
+import React, { Component } from 'react';
+import CampsiteInfo from './CampsiteInfoComponent';
+import Directory from './DirectoryComponent';
+import { CAMPSITES } from '../shared/campsites';
+import Header from './HeaderComponent';
+import Footer from './FooterComponent';
+import Home from './HomeComponent';
+import { Switch, Route, Redirect } from 'react-router-dom';
+
+
+class Main extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            campsites: CAMPSITES
+        };
+    }
+
+    render() {
+
+        const HomePage = () => {
+            return (
+                <Home />
+            );
+        };
+
+        return (
+            <div>
+                <Header />
+                <Switch>
+                    <Route path='/home' component={HomePage} />
+                    <Route exact path='/directory' render={() => <Directory campsites={this.state.campsites} />} />
+                    <Redirect to='/home' />
+                </Switch>
+                <Footer />
+            </div>
+        );
+
+    
+   }
+  }
+
+
+
+export default Main;
